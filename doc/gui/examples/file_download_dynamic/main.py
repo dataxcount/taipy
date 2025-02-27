34853231c488ee95489c7e7d7bed4da537b8535d
dataxcount <dataxcount@gmail.com>
2025-02-27 11:42:29 +0100

    Example for issue #2450
    Illustrate an issue when calling a cleanup callback after downloading a file in the particular case of an appli organised for multipages :
    clean_up callback can not be called in this case : "\Lib\site-packages\taipy\gui\gui.py:1511: TaipyGuiWarning: on_action(): 'tp_clean_up_0' is not a valid function."
    ... as explained in issue #2450

---
+++ doc/gui/examples/file_download_dynamic/main.py
@@ -0,0 +1,17 @@
+import taipy as tp
+from taipy.gui import Gui
+from pages.pages_grp1.page1.page1 import page1_md
+from pages.root import root
+
+pages = {
+    '/':root,
+    'etablissements/actusXY':page1_md,
+}
+
+lov_navbar = [("/pages_grp1/page1", "Example 1")]
+
+if __name__ == '__main__':
+    app = Gui(pages=pages)
+    tp.Orchestrator().run()
+    app.run(title="TSSvisu", use_reloader=True, port=5555)
+    
